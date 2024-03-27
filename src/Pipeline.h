752272ce3981963054e23bac57f89e1dfedeeb7b
Rui Cruz <rpsc16@gmail.com>
2024-03-27 17:22:28 +0000

    Documentacao praticamente feita

--- src/Pipeline.h
+++ src/Pipeline.h
@@ -3,23 +3,47 @@
 
 #include <string>
 
+/**
+ * @brief Classe que representa um tubo de água
+ */
+
 class Pipeline {
 private:
+    /// Serviço de origem de um tubo de água
     std::string _sourc;
 
+    /// Serviço alvo de um tubo de água
     std::string _dest;
 
+    /// Capacidade de um tubo de água
     int _capacity;
 
+    /// Direção de um tubo de água ( 1 - Unidiretional | 0 - Bidiretional)
     int _direction;
 
 public:
+    /**
+     * @brief Retorna o serviço de origem de um tubo de água
+     * @return Serviço de origem de um tubo de água
+     */
     [[nodiscard]] std::string getSourc() const;
 
+    /**
+     * @brief Retorna o serviço alvo de um tubo de água
+     * @return Serviço alvo de um tubo de água
+     */
     [[nodiscard]] std::string getDest() const;
 
+    /**
+     * @brief Retorna a capacidade de um tubo de água
+     * @return Capacidade de um tubo de água
+     */
     [[nodiscard]] int getCapacity() const;
 
+    /**
+     * @brief Retorna a direção de um tubo de água
+     * @return Direção de um tubo de água
+     */
     [[nodiscard]] int getDirection() const;
 };
 
--- src/Station.h
+++ src/Station.h
@@ -3,17 +3,36 @@
 
 #include <string>
 
+/**
+ * @brief Classe que representa uma estação de bombeamento
+ */
+
 class Station {
 private:
+    /// Identificador único da estação de bombeamento
     std::string _id;
 
+    /// Código da estação de bombeamento
     std:: string _code;
 
 public:
+    /**
+     * @brief Retorna o identificador único da estação de bombeamento
+     * @return Identificador único da estação de bombeamento
+     */
     [[nodiscard]] std::string getId() const;
 
+    /**
+     * @brief Retorna o código da estação de bombeamento
+     * @return Código da estação de bombeamento
+     */
     [[nodiscard]] std::string getCode() const;
 
+    /**
+     * @brief Constructor da classe Station
+     * @param id Identificador único da estação de bombeamento
+     * @param code Código da estação de bombeamento
+     */
     Station (std::string id, std::string code) : _id(std::move(id)), _code(std::move(code)) {};
 };
 
